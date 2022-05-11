4acae6304421c7832181ffdb209cb3cd9825d941
Luke Kreczko <lkreczko@googlemail.com>
2022-05-11 08:04:43 +0100

    added cbsensor ganglia module

---
+++ cbsensor/README.md
@@ -0,0 +1,3 @@
+# cbsensor
+Python module for ganglia 3.7+.
+`cbsensor` sends metrics on `cbsensor` and `cbdaemon` to ganglia.
---
+++ cbsensor/python_modules/ganglia_cbsensor.py
@@ -0,0 +1,88 @@
+#!/opt/conda/bin/python
+import psutil
+import time
+
+last_update = 0
+descriptors = list()
+cbsensor_stats = dict()
+
+MIN_UPDATE_INTERVAL = 30  # seconds
+METRIC_PREFIX = "cbsensor_"
+CB_PROCESS_NAMES = ["cbsensor", "cbdaemon", "python"]
+METRIC_DESCRIPTORS = {
+    "cpu_percent": {
+        "units": "%",
+        "value_type": "double",
+        "slope": "both",
+        "format": "%.2f",
+        "description": "CPU usage in percent",
+        "groups": ["cbsensor"],
+    },
+    "memory_percent": {
+        "units": "%",
+        "value_type": "double",
+        "slope": "both",
+        "format": "%.2f",
+        "description": "Memory usage in percent",
+        "groups": ["cbsensor"],
+    },
+    "nproc": {
+        "units": "",
+        "value_type": "uint32",
+        "slope": "both",
+        "format": "%u",
+        "description": "Number of processes",
+        "groups": ["cbsensor"],
+    },
+}
+
+
+def get_cbsensor_metrics():
+    global last_update
+    global cbsensor_stats
+
+    now = time.time()
+    if now - last_update < MIN_UPDATE_INTERVAL:
+        return True
+    last_update = now
+
+    selected_processes = [proc for proc in psutil.process_iter() if proc.name() in CB_PROCESS_NAMES]
+    cbsensor_stats = dict(
+        nproc=len(selected_processes),
+        cpu_percent=sum(proc.cpu_percent() for proc in selected_processes),
+        memory_percent=sum(proc.memory_percent() for proc in selected_processes),
+    )
+    return True
+
+
+def get_cbsensor_metric(name: str):
+    global cbsensor_stats
+    if METRIC_PREFIX in name:
+        name = name.replace(METRIC_PREFIX, "")
+    return cbsensor_stats.get(name, 0)
+
+
+def metric_init(params):
+    global cbsensor_stats
+    global descriptors
+
+    get_cbsensor_metrics()
+
+    descriptors = list()
+    for name, metric in cbsensor_stats.items():
+        metric = METRIC_DESCRIPTORS[name].copy()
+        metric["name"] = METRIC_PREFIX + name
+        metric["call_back"] = get_cbsensor_metric
+        descriptors.append(metric)
+    return descriptors
+
+
+def metric_cleanup():
+    pass
+
+
+if __name__ == '__main__':
+    metric_init({})
+    for d in descriptors:
+        v = d['call_back'](d['name'])
+        print(f"value for {d['name']} is {v}")
